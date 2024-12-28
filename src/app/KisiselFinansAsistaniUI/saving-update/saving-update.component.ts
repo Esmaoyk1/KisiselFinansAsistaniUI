e2442bd30c42eb72941c347b772e0aab5637c0ad
esmanur oyanık <esmaoyanik55@gmail.com>
2024-12-28 23:04:05 +0300

    saving-update klasörü eklenmiyor

---
+++ src/app/KisiselFinansAsistaniUI/saving-update/saving-update.component.ts
@@ -0,0 +1,138 @@
+//import { Component, OnInit } from '@angular/core';
+//import { ParamMap, Router, RouterModule } from '@angular/router';
+//import { MenuComponent } from '../../menu/menu.component';
+//import { ActivatedRoute } from '@angular/router';
+//import { FormsModule } from '@angular/forms';
+//@Component({
+//  selector: 'app-saving-update',
+//  standalone: true,
+//  imports: [RouterModule, MenuComponent, FormsModule],
+//  templateUrl: './saving-update.component.html',
+//  styleUrl: './saving-update.component.css'
+//})
+
+//export class SavingUpdateComponent implements OnInit {
+//  hedefId: number | null = null; // Başlangıç değeri olarak null verildi
+//  post: any;
+
+//  constructor(private route: ActivatedRoute, private router: Router) {
+//    const navigation = this.router.getCurrentNavigation();
+//    this.post = null;
+//    if (navigation && navigation.extras.state) {
+//      this.post = navigation.extras.state['post'];
+//      this.post.targetDate = new Date(this.post.targetDate).toISOString().split('T')[0];
+
+//      console.log(this.post);
+//      } else {
+//        console.warn('Navigation veya state bulunamadı.');
+//        this.post = {}; // Varsayılan değer
+//      }
+//  }
+//  ngOnInit() {
+
+//  }
+//  ngOnDestroy(): void {
+//    // Bileşen yok edilirken durumu temizle
+//    this.post = null;
+//  }
+//}
+
+
+
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { FormsModule, NgForm } from '@angular/forms'; // FormsModule'ü buradan içe aktarın
+import { SavingApiService } from '../../services/saving.service';
+import { HttpClientModule } from '@angular/common/http'; // İçe aktarın
+@Component({
+  selector: 'app-saving-update',
+  standalone: true,
+  imports: [RouterModule, FormsModule,HttpClientModule], // Burada FormsModule'ü ekleyin
+  templateUrl: './saving-update.component.html',
+  styleUrls: ['./saving-update.component.css']
+})
+export class SavingUpdateComponent implements OnInit {
+  hedefId: number | null = null;
+  post: any;
+
+  constructor(private route: ActivatedRoute, private router: Router, private savingApiService: SavingApiService) {
+    const navigation = this.router.getCurrentNavigation();
+    if (navigation && navigation.extras.state) {
+      this.post = navigation.extras.state['post'];
+      this.post.targetDate = new Date(this.post.targetDate).toISOString().split('T')[0];
+    } else {
+      console.warn('Navigation veya state bulunamadı.');
+      this.post = {};
+    }
+  }
+
+  ngOnInit() {
+    this.hedefId = this.post.id; // Eğer post içinde id varsa
+  }
+
+  onUpdate(form: NgForm) {
+    if (form.valid && this.hedefId) {
+      const updatedData = {
+        goalName: this.post.goalName,
+        goalAmount: this.post.goalAmount,
+        savedAmount: this.post.savedAmount,
+        targetDate: this.post.targetDate
+      };
+
+      this.savingApiService.updatePost(this.hedefId, updatedData).subscribe(response => {
+        console.log('Başarıyla güncellendi:', response);
+        alert('Güncelleme işlemi başarıyla tamamlandı!');
+        this.router.navigate(['/saving']);
+      }, error => {
+       
+        console.error('Güncelleme hatası:', error);
+        alert('Güncelleme sırasında bir hata oluştu.');
+      });
+    } else {
+      console.log('Form geçersiz.');
+    }
+  }
+}
+
+
+
+//import { Component, OnInit } from '@angular/core';
+//import { ParamMap, Router, RouterModule } from '@angular/router';
+//import { MenuComponent } from '../../menu/menu.component';
+//import { ActivatedRoute } from '@angular/router';
+//import { FormsModule } from '@angular/forms';
+//@Component({
+//  selector: 'app-saving-update',
+//  standalone: true,
+//  imports: [RouterModule, MenuComponent, FormsModule],
+//  templateUrl: './saving-update.component.html',
+//  styleUrl: './saving-update.component.css'
+//})
+
+//export class SavingUpdateComponent implements OnInit {
+//  hedefId: number | null = null; // Başlangıç değeri olarak null verildi
+//  post: any;
+
+//  constructor(private route: ActivatedRoute, private router: Router) {
+//    const navigation = this.router.getCurrentNavigation();
+//    this.post = null;
+//    if (navigation && navigation.extras.state) {
+//      this.post = navigation.extras.state['post'];
+//      this.post.targetDate = new Date(this.post.targetDate).toISOString().split('T')[0];
+
+//      console.log(this.post);
+//    } else {
+//      console.warn('Navigation veya state bulunamadı.');
+//      this.post = {}; // Varsayılan değer
+//    }
+//  }
+//  ngOnInit() {
+
+//  }
+//  ngOnDestroy(): void {
+//    // Bileşen yok edilirken durumu temizle
+//    this.post = null;
+//  }
+
+
+//}
