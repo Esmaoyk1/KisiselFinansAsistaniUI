490cf3634a07f1db07c549688626a5a5e8bec34c
esmanur oyanık <esmaoyanik55@gmail.com>
2024-12-28 23:04:43 +0300

    saving-delete klasörü eklenmiyor

---
+++ src/app/KisiselFinansAsistaniUI/saving-delete/saving-delete.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-saving-delete',
+  standalone: true,
+  imports: [],
+  templateUrl: './saving-delete.component.html',
+  styleUrl: './saving-delete.component.css'
+})
+export class SavingDeleteComponent {
+
+}
