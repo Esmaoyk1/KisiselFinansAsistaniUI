f47e248d024db22940138ac6fe25b485bd51956f
esmanur oyanık <esmaoyanik55@gmail.com>
2025-01-02 16:10:44 +0300

    js ler Asenkron a çevrildi

--- src/app/KisiselFinansAsistaniUI/budget/budget.component.ts
+++ src/app/KisiselFinansAsistaniUI/budget/budget.component.ts
@@ -9,24 +9,53 @@ import { Component, Renderer2 } from '@angular/core';
 })
 export class BudgetComponent {
   ngOnInit() {
+    this.loadScriptsSequentially();
+
     this.loadCss('assets/vendor/datatables/dataTables.bootstrap4.min.css');
 
-    this.loadScript('assets/js/test.js');
-    this.loadScript('assets/vendor/jquery/jquery.min.js');
-    this.loadScript('assets/vendor/datatables/jquery.dataTables.min.js');
-    this.loadScript('assets/vendor/datatables/dataTables.bootstrap4.min.js');
-    this.loadScript('assets/js/demo/datatables-demo.js');
+    //this.loadScript('assets/js/test.js');
+    //this.loadScript('assets/vendor/jquery/jquery.min.js');
+    //this.loadScript('assets/vendor/datatables/jquery.dataTables.min.js');
+    //this.loadScript('assets/vendor/datatables/dataTables.bootstrap4.min.js');
+    //this.loadScript('assets/js/demo/datatables-demo.js');
 
   }
 
   constructor(private renderer: Renderer2) { }
-  private loadScript(src: string): void {
-    const script = this.renderer.createElement('script');
-    script.src = src;
-    script.type = 'text/javascript';
-    script.async = true;
-    this.renderer.appendChild(document.body, script);
+  async loadScriptsSequentially() {
+    try {
+      await this.loadScript('assets/vendor/jquery/jquery.min.js');
+      console.log('jQuery yüklendi.');
+      await this.loadScript('assets/vendor/datatables/jquery.dataTables.min.js');
+      console.log('DataTables JS yüklendi.');
+      await this.loadScript('assets/vendor/datatables/dataTables.bootstrap4.min.js');
+      console.log('Bootstrap DataTables yüklendi.');
+      await this.loadScript('assets/js/demo/datatables-demo.js');
+      console.log('Demo script yüklendi.');
+      await this.loadScript('assets/js/test.js');
+      console.log('Test script yüklendi.');
+    } catch (error) {
+      console.error(error);
+    }
   }
+  loadScript(scriptUrl: string): Promise<void> {
+    return new Promise((resolve, reject) => {
+      const script = document.createElement('script');
+      script.src = scriptUrl;
+      script.async = true;
+      script.onload = () => resolve();
+      script.onerror = () => reject(`Script yüklenemedi: ${scriptUrl}`);
+      document.body.appendChild(script);
+    });
+  }
+
+  //private loadScript(src: string): void {
+  //  const script = this.renderer.createElement('script');
+  //  script.src = src;
+  //  script.type = 'text/javascript';
+  //  script.async = true;
+  //  this.renderer.appendChild(document.body, script);
+  //}
   private loadCss(url: string) {
     const link = this.renderer.createElement('link');
     link.rel = 'stylesheet';
